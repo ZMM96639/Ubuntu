17f99bc7c761abcdc33b66d99edad3cc6a436c1e
ZMM <15115238791@163.com>
2021-05-11 00:22:56 +0800

    dubge

--- imshow/inshow.cpp
+++ imshow/inshow.cpp
@@ -1,29 +1,37 @@
 //
 // Created by zmm on 5/5/21.
 //
+#include <iostream>
 #include <opencv4/opencv2/core.hpp>
 #include <opencv4/opencv2/highgui.hpp>
+
+using namespace std;
 using namespace cv;
 
-int main(int argc,char **argv){
-    Mat girl = imread("/home/meimiao/Pictures/1.jpg");
-    namedWindow("1picture");
-    imshow("1picture",girl);
-    Mat img = imread("/home/meimiao/Pictures/2.jpg",199);
-    Mat logo = imread("2_logo.jpg");
-    namedWindow("2.jpg");
-    imshow("2.jpg",img);
-    namedWindow("3logo");
-    imshow("3logo", logo);
+int main(int argc, char **argv) {
+//    Mat girl = imread(argv[1]);
+//    namedWindow("1picture", WINDOW_AUTOSIZE);
+//    imshow("1picture", girl);
+
+    Mat img = imread(argv[2]);
+    Mat logo = imread(argv[3]);
+//    namedWindow("2.jpg", WINDOW_AUTOSIZE);
+//    imshow("2.jpg", img);
+//
+//    namedWindow("3logo");
+//    imshow("3logo", logo);
+
     Mat imgROI;
 //first way
-imgROI = image(Rect(800,350,logo.cols,logo.rows));
+    cout << "hello" << endl;
+    imgROI = img(Rect(800, 350, logo.cols, logo.rows));
+    cout << "hello" << endl;
 //second way
-imgROI = image(Rang(350,350+logo.rows),Rang(800,800+logo.cols));
-    addWeighted(imgROI,0.5,logo,0.3,0,imgROI);
+//    imgROI = img(Range(350, 350 + logo.rows), Range(800, 800 + logo.cols));
+    addWeighted(imgROI, 0.5, logo, 0.3, 0, imgROI);
     namedWindow("4picture");
-    imshow("4picture",img);
+    imshow("4picture", img);
     waitKey();
-    return 0;
 
+    return 0;
 }
