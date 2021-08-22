e29de1e2b36ac1b318dc6ae7deda918818e42c73
ZMM <15115238791@163.com>
2021-08-22 23:25:04 +0800

    窗口调参

---
+++ sgbm/cmake-build-debug/CMakeFiles/sgbm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sgbm.dir/main.cpp.o"
+  "sgbm"
+  "sgbm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sgbm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
