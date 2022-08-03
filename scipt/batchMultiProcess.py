07f995ed799868c75b9eace074a135b25f4466c6
ZMM <15115238791@163.com>
2022-08-03 18:04:08 +0800

    multiProcess

--- scipt/batchMultiProcess.py
+++ scipt/batchMultiProcess.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-import threading
+from multiprocessing import Process
 
 
 def execute_command(command):
@@ -21,22 +21,22 @@ def execute_command(command):
 # TODO too many params, try e.g. -i "--readers.text.filename=/test/*.txt" -o
 # TODO  multile threads parallel
 def pdal_batch_process(odir, writer_name, oext, json_path, options):
-    threads = []
+    procssList = []
     for num in range(1, 18, 2):
         i = num / 100000
         output_file = odir + str(i) + oext
         command = " pdal pipeline -i " + json_path + " --filters.voxeldownsize.cell=" + str(i) + \
                   " --writers." + writer_name + ".filename=" + output_file + " " + options
 
+        p = Process(target=execute_command, args=(command,))
+        procssList.append(p)
+        p.start()
+
         print("/*************************************/")
         print(command)
 
-        t = threading.Thread(target=execute_command, args=(command,))
-        t.start()
-        threads.append(t)
-
-    for th in threads:
-        th.join()
+    for pr in procssList:
+        pr.join()
 
 
 if __name__ == '__main__':
